c24de4adaab958e6eca5b27dc95da92e13b89393
ponleur123 <ponleurveng@gmail.com>
2022-03-25 10:05:45 +0700

    second commit

---
+++ ex1-login-cookie/src/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+var userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        unique:true,
+        require:true
+    },
+    password:{
+        type:String
+    },
+    rpasswrod:{
+        type:String
+    }
+},{
+    timestamps:true,
+});
+
+var Users = mongoose.model('Users',userSchema)
+module.exports = Users
